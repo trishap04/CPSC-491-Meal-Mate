fae8feda29ab480c8dcd7250698b3a1dede6a880
Batu-end <ba2demirtas@gmail.com>
2026-04-03 19:48:09 -0700

    MEAL-17: feat: define UserProfile database schema extending BaseUser and apply SQL migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'users',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -115,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# CORS Configuration for allowing Frontend access
+CORS_ALLOW_ALL_ORIGINS = True
