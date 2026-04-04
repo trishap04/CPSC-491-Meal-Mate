4ec4e83dabafc2dc9b4b7cdd56a45bd8278238c7
Batu-end <ba2demirtas@gmail.com>
2026-04-03 19:55:25 -0700

    MEAL-95: Update on database, serializers and views

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "message": "User registered successfully!",
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email
+                }
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
